4882fa1a1708ceab0fadd2a5a0c05c4c8c9d33a0
yaara <yaara3332@gmail.com>
2025-12-01 09:37:27 +0200

    3 commit

--- app/src/main/java/com/example/ex01421/MainActivity.java
+++ app/src/main/java/com/example/ex01421/MainActivity.java
@@ -19,6 +19,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // הערה
+        //הערה
     }
 }
